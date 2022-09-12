ecf60ff71461c7eea6670145ead17010926a3c6f
wchizewski <90284175+wchizewski@users.noreply.github.com>
2022-09-12 14:20:47 -0600

    done

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
 // Triangle Perimeter Assignment Start Code
-let
+document.getElementById("calc").addEventListener("click", calculate)
+
+function calculate() {
+    let xA = +document.getElementById("xA").value;
+    let yA = +document.getElementById("yA").value;
+    let xB = +document.getElementById("xB").value;
+    let yB = +document.getElementById("yB").value;
+    let xC = +document.getElementById("xC").value;
+    let yC = +document.getElementById("yC").value;
+
+    let AB = dist(xA, yA, xB, yB);
+    let AC = dist(xA, yA, xC, yC);
+    let BC = dist(xB, yB, xC, yC);
+    let perimeter = AB + AC + BC;
+
+    document.getElementById("ab").innerHTML = "AB = " + AB
+    document.getElementById("ac").innerHTML = "AC = " + AC
+    document.getElementById("bc").innerHTML = "BC = " + BC
+    document.getElementById("perimeter").innerHTML = "Perimeter of ABC = " + perimeter
+
+
+}
+
+function dist(x1, y1, x2, y2) {
+    return Math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+}
